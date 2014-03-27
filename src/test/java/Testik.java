7f610ce22e8a0ed449ad94093475759c9acf4a88
oleksandrpo <alexandrpolishchuk29@gmail.com>
2014-03-27 15:29:40 +0200

    add timeout

--- src/test/java/Testik.java
+++ src/test/java/Testik.java
@@ -20,23 +20,24 @@ public class Testik {
 //        driver.findElement(By.id("quick_pass")).sendKeys("Polishchuk0209Q");
 //        driver.findElement(By.id("quick_login_button")).click();
 //        driver.close();
-//        System.out.println();
+//        System.out.println();      play_new playing
 //        Assert.assertTrue(2==2);
 //    }
 
 
     @Test
-    public void test2() {
+    public void test2() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.get("http://vk.com");
         if (driver.findElement(By.id("quick_email")) != null) {
             driver.findElement(By.id("quick_email")).sendKeys("stydentkpi@meta.ua");
-            driver.findElement(By.id("quick_pass")).sendKeys("Polishchuk0209Q");
+            driver.findElement(By.id("quick_pass")).sendKeys("polishchuk9379992qwerty");
             driver.findElement(By.id("quick_login_button")).click();
             driver.findElement(By.id("head_music_text")).click();
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            driver.findElement(By.id("pad_search")).sendKeys("Михаил круг");
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+
+            driver.findElement(By.id("pad_search")).sendKeys("Михаил Круг");
+            Thread.sleep(3000);
             driver.findElement(By.className("play_btn_wrap")).click();
             System.out.println();
 
